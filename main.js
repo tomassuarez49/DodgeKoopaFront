b664c38f5e877a49effda956134c6a1a3cc8f174
N1CKZ3V <nico_03_sebas@hotmail.com>
2024-12-03 20:50:36 -0500

    Intento 2 conexion

--- main.js
+++ main.js
@@ -235,7 +235,7 @@ function handleServerMessage(event) {
 
 
 function initializeWebSocket() {
-    socket = new WebSocket('wss://13.89.39.238');// Cambia localhost si el servidor está en otro lugar
+    socket = new WebSocket('ws://koopabalancer.centralus.cloudapp.azure.com');// Cambia localhost si el servidor está en otro lugar
 
     socket.onopen = () => {
         console.log('WebSocket conectado.');
