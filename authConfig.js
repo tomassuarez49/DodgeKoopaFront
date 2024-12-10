77d35dc6bb3a5a786c054cd02b4981429fbd2e02
tomassuarez49 <tomas.suarez@mail.escuelaing.edu.co>
2024-12-10 15:41:07 -0500

    link authConfig change

--- authConfig.js
+++ authConfig.js
@@ -1,6 +1,6 @@
 export async function getMsalConfig() {
     // Cambia la URL a la de tu backend desplegado
-    const response = await fetch('dodgekoopaback-cgc4grgdefhxakav.centralus-01.azurewebsites.net');
+    const response = await fetch('https://dodgekoopaback-cgc4grgdefhxakav.centralus-01.azurewebsites.net');
     if (!response.ok) {
         throw new Error('Error al obtener el Client ID desde el backend');
     }
